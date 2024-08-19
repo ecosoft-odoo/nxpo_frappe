e031f5f9549a809f7be35d8616a48e0fed74cb08
Your Name <you@example.com>
2024-08-19 17:01:52 +0000

    [FIX] install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,14 @@ export PULL_POLICY="never"
 # --
 export ADMIN_PASSWORD="admin"
 export DB_PASSWORD="nxpopwd"
-export LETSENCRYPT_EMAIL="yongyut@nxpo.or.th"
-export SITES="people.nxpo.or.th","people-test.nxpo.or.th"
+export SITES=("people.nxpo.or.th" "people-test.nxpo.or.th")
 
 # Next image tag = 1.<minor version>
 # When run this script, minor version will plus 1 from original image
-latest_image=$(docker images --filter=reference="$CUSTOM_IMAGE:*" --format "{{.Repository}}:{{.Tag}}" | sort -r | head -n 1)
-if [ -n "$latest_image" ]; then
-    major_version=$(echo ${latest_image#*:} | cut -d '.' -f 1)
-    minor_version=$(echo ${latest_image#*:} | cut -d '.' -f 2)
+origin_image=$(docker images --filter=reference="$CUSTOM_IMAGE:*" --format "{{.Repository}}:{{.Tag}}" | sort -r | head -n 1)
+if [ -n "$origin_image" ]; then
+    major_version=$(echo ${origin_image#*:} | cut -d '.' -f 1)
+    minor_version=$(echo ${origin_image#*:} | cut -d '.' -f 2)
     export CUSTOM_TAG="$major_version.$((minor_version + 1))"
 fi
 
@@ -36,13 +35,16 @@ docker build --no-cache --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 --tag=$CU
 # Start frappe
 image_from_build=$(docker images --filter=reference="$CUSTOM_IMAGE:$CUSTOM_TAG" --format "{{.Repository}}:{{.Tag}}")
 if [ -n "$image_from_build" ]; then
-    docker compose --project-name nxpo-frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f overrides/compose.https.yaml config > ../docker-compose.yaml
-    docker compose --project-name nxpo-frappe -f ../docker-compose.yaml up -d --force-recreate
-    echo "$SITES" | tr ',' '\n' | while read -r site;
+    docker compose --project-name nxpo-frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f ../overrides/compose.https.yaml config > ../docker-compose.yaml
+    docker compose --project-name nxpo-frappe -f ../docker-compose.yaml down
+    docker compose --project-name nxpo-frappe -f ../docker-compose.yaml up -d
+    for site in "${SITES[@]}";
     do
         docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec -T backend bench new-site $site --no-mariadb-socket --mariadb-root-password $DB_PASSWORD --admin-password $ADMIN_PASSWORD
         docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec -T backend bench --site $site install-app erpnext
     done
+else
+    echo "Build new image failed !!"
 fi
 
 # Remove all images not used in container
