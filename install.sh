c1789ac2f902e978f7de409b9462e7836c8a2ec8
Tharathip C. @ Ecosoft <tharathip.chaweewongphan@gmail.com>
2024-08-19 11:23:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ export PULL_POLICY="never"
 # --
 export ADMIN_PASSWORD="admin"
 export DB_PASSWORD="nxpopwd"
-export SITE="10.1.20.68"
+export SITES="people.nxpo.or.th","people-test.nxpo.or.th"
 
 # Next image tag = 1.<minor version>
 # When run this script, minor version will plus 1 from original image
@@ -35,11 +35,15 @@ docker build --no-cache --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 --tag=$CU
 # Start frappe
 image_from_build=$(docker images --filter=reference="$CUSTOM_IMAGE:$CUSTOM_TAG" --format "{{.Repository}}:{{.Tag}}")
 if [ -n "$image_from_build" ]; then
-    docker compose --project-name nxpo-frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f overrides/compose.proxy.yaml config > ../docker-compose.yaml
+    docker compose --project-name nxpo-frappe -f compose.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml -f overrides/compose.https.yaml config > ../docker-compose.yaml
     docker compose --project-name nxpo-frappe -f ../docker-compose.yaml down
     docker compose --project-name nxpo-frappe -f ../docker-compose.yaml up -d
-    docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec backend bench new-site $SITE --no-mariadb-socket --mariadb-root-password $DB_PASSWORD --admin-password $ADMIN_PASSWORD
-    docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec backend bench --site $SITE install-app erpnext
+    IFS="," read -r -a site_list <<< "$SITES"
+    for site in "${site_list[@]}"
+    do
+        docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec backend bench new-site $site --no-mariadb-socket --mariadb-root-password $DB_PASSWORD --admin-password $ADMIN_PASSWORD
+        docker compose --project-name nxpo-frappe -f ../docker-compose.yaml exec backend bench --site $site install-app erpnext
+    done
 fi
 
 # Remove all images not used in container
